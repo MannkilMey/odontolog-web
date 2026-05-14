49e2fc95da952b18dd1639eefbc0e366e240b617
Jose Benitez Meyer <jcbenitezmeyer@gmail.com>
2026-05-14 19:14:27 -0300

    features: translate pages

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -1,4 +1,3 @@
-// ANTES:
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
@@ -17,37 +16,9 @@ i18n
       pt: { translation: pt }
     },
     fallbackLng: 'es',
-    interpolation: { escapeValue: false },
-    detection: {
-      order: ['localStorage', 'navigator'],
-      caches: ['localStorage'],
-      lookupLocalStorage: 'odontolog_idioma'
-    }
-  })
-
-export default i18n  // ← ELIMINAR ESTA LÍNEA
-
-
-// DESPUÉS:
-import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
-import LanguageDetector from 'i18next-browser-languagedetector'
-
-import es from './locales/es.json'
-import en from './locales/en.json'
-import pt from './locales/pt.json'
-
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources: {
-      es: { translation: es },
-      en: { translation: en },
-      pt: { translation: pt }
+    interpolation: {
+      escapeValue: false
     },
-    fallbackLng: 'es',
-    interpolation: { escapeValue: false },
     detection: {
       order: ['localStorage', 'navigator'],
       caches: ['localStorage'],
